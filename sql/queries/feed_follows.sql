937aef02126c5a21c2246370c9f8e945a2d5a584
MSamoilovic <msamoilovic@gmail.com>
2026-07-29 14:29:52 +0200

    fix(feed): fix for duplicate feeds

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -1,7 +1,8 @@
--- name: CreateFeedFollow :one
+-- name: CreateFeedFollow :many
 WITH inserted AS (
     INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
     VALUES ($1, $2, $3, $4, $5)
+    ON CONFLICT (user_id, feed_id) DO NOTHING
     RETURNING *
 )
 SELECT
